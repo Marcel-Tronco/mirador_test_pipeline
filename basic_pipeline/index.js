7a0a6ded110ea41c107eccdec571d3e276b24478
Marcel Tronco <Marcel.Tronco@posteo.de>
2025-02-13 13:13:49 +0100

    first trial for build pipeline

---
+++ basic_pipeline/index.js
@@ -0,0 +1,13 @@
+import Mirador from 'mirador'
+import { miradorImageToolsPlugin } from 'mirador-image-tools';
+import textOverlayPlugin from 'mirador-textoverlay/es';
+import downloadDialogPlugin from 'mirador-downloaddialog/es'
+
+window.MiradorBundle = {
+    Mirador,
+    plugins: {
+        miradorImageToolsPlugin,
+        textOverlayPlugin,
+        downloadDialogPlugin
+    }
+};
