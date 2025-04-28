81a9013caa73fb8c5e56e4b55e185fca7e1f2869
Marcel 🪵 <69786342+Marcel-Tronco@users.noreply.github.com>
2025-04-28 16:19:17 +0200

    Update index.js

--- basic_pipeline/index.js
+++ basic_pipeline/index.js
@@ -1,13 +1,13 @@
 import Mirador from 'mirador'
 import { miradorImageToolsPlugin } from 'mirador-image-tools';
 import textOverlayPlugin from 'mirador-textoverlay/es';
-import downloadDialogPlugin from 'mirador-downloaddialog/es'
+// import downloadDialogPlugin from 'mirador-downloaddialog/es'
 
 window.MiradorBundle = {
     Mirador,
     plugins: {
         miradorImageToolsPlugin,
         textOverlayPlugin,
-        downloadDialogPlugin
+        // downloadDialogPlugin
     }
 };
